7108689ad137979b195a7d062006ad353ccd5bff
woojin <orolzleim@gmail.com>
2024-05-06 23:14:26 +0900

    BE-initial setup

---
+++ src/main/java/com/example/kidsconnect/KidsconnectApplication.java
@@ -0,0 +1,20 @@
+package com.example.kidsconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class KidsconnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KidsconnectApplication.class, args);
+    }
+
+    @GetMapping("/hi")
+    public String hello(){
+        return "hello";
+    }
+}
---
+++ src/test/java/com/example/kidsconnect/KidsconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kidsconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidsconnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
