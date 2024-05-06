84f40c45a1d13c89118e44ca3995f364c7483215
heoh0613 <heoh0613@gmail.com>
2024-05-06 23:08:41 +0900

    [FE]Default_Buildup/FileTree/npm/eslint Done

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        얘는 냅두고 아래에 Component 연결 후 url 넣기 이친구는 나중에 같이 삭제
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
