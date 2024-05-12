f795102712593aa02e81e7fa3ae8cd88c40b96f7
heoh0613 <heoh0613@gmail.com>
2024-05-12 16:32:21 +0900

    [FE]DesignSystem에 필요한 데이터, css, 로고 이미지 추가

---
+++ client/src/Assets/StyledData/ColorTheme.ts
@@ -0,0 +1,24 @@
+const ColorTheme: object = {
+  primaryColor: "#FF7000",
+  secondaryColor: "#0038FF",
+  pcDark: "#D45E00",
+  pcLight1: "#FFB274",
+  pcLight2: "#FFD8B8",
+  scDark: "#042188",
+  scLight1: "#6889FF",
+  scLight2: "#C1CFFF",
+  subGreen: "#BBE7B7",
+  subRed: "#F8B6B6",
+  subYellow: "#F2EAA0",
+  subPink: "#FED5FF",
+  subDarkGreen: "#B5D7C1",
+  black: "#333333",
+  gray1: "#666666",
+  gray2: "#999999",
+  lightGray1: "#C1C1C1",
+  lightGray2: "#F2F2F2",
+  white: "#FFFFFF",
+  error: "#FF2727",
+};
+
+export default ColorTheme;
