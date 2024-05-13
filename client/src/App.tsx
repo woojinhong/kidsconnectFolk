cf146952b1fb584c3c61d1f4a03a6bca5df3c399
heoh0613 <heoh0613@gmail.com>
2024-05-13 17:46:36 +0900

    [chore] App.tsx 원상복귀

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter } from "react-router-dom";
-import Header from "./Component/Common/Header/Header";
 
 function App() {
   return (
@@ -7,7 +6,6 @@ function App() {
       <div>
         얘는 냅두고 아래에 Component 연결 후 url 넣기 이친구는 나중에 같이 삭제
       </div>
-      <Header />
     </BrowserRouter>
   );
 }
