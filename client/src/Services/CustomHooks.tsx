a16577d42a87bab085a4ea2ad0c5cdd68bd29b7f
heoh0613 <heoh0613@gmail.com>
2024-05-23 15:35:37 +0900

    [FE] ChildListCard 컴포넌트 구현

--- client/src/Services/CustomHooks.tsx
+++ client/src/Services/CustomHooks.tsx
@@ -1,3 +1,4 @@
+// 경력 년,월 시작일, 끝난일을 통해 계산 커스텀 훅
 export function useCareerDateCalc(startDate: string, endDate: string) {
   const careerDateYear =
     new Date(endDate).getFullYear() - new Date(startDate).getFullYear();
@@ -14,4 +15,16 @@ export function useCareerDateCalc(startDate: string, endDate: string) {
   } else {
     return `${careerDateYear}년 ${careerDateMonth}개월`;
   }
+}
+
+// 아이 나이 태어난 날짜를 통해 계산 커스텀 훅
+export function useGetChildAge(dateOfBirth: string) {
+  const today = new Date();
+  const birthDate = new Date(dateOfBirth);
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const month = today.getMonth() - birthDate.getMonth();
+  if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
 }
