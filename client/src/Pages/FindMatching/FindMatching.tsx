affc0702f1fee141f6c6ecc9b3558b7d0258df58
heoh0613 <heoh0613@gmail.com>
2024-06-12 12:25:13 +0900

    [Fix] Add default Checkbox value and Category

--- client/src/Pages/FindMatching/FindMatching.tsx
+++ client/src/Pages/FindMatching/FindMatching.tsx
@@ -1,10 +1,43 @@
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+
 import Category from "../../Component/Common/Category/Category";
 import SelectBoxDefault from "../../Component/Common/SelectBox/SelectBoxDefault";
 import TherapistCard from "../../Component/Common/Card/TherapistCard/TherapistCard";
+
+import { PreferenceSurveyState } from "../../Store/Slices/MatchingSurveySlice";
 import treatmentAreaText from "../../Assets/TextData/treatmentAreaText";
+import { getSelectedTreatmentArea } from "../../Services/CustomHooks";
+
+interface RootState {
+  preferenceData: PreferenceSurveyState;
+}
 
 function FindMatching() {
+  const [surveyedData, setSurveyedData] = useState<PreferenceSurveyState>();
+  const [selectedTreatmentArea, setSelectedTreatmentArea] = useState<string[]>(
+    []
+  );
+  const [selectedRegion, setSelectedRegion] = useState<string | null>();
+  const [selectedGender, setSelectedGender] = useState<string | null>();
+  const [selectedCareer, setSelectedCareer] = useState<
+    string | boolean | null
+  >();
+  const [selectedFilterBy, setSelectedFilterBy] = useState<string | null>();
+
+  const checkedSurveyData = useSelector(
+    (state: RootState) => state.preferenceData
+  );
+
+  useEffect(() => {
+    setSurveyedData(checkedSurveyData);
+    if (checkedSurveyData.treatmentArea) {
+      setSelectedTreatmentArea(checkedSurveyData.treatmentArea);
+    }
+  }, []);
+  // 추후 api
   const therapistId: number[] = [1, 2, 3, 4];
+
   return (
     <main>
       <section>
@@ -15,15 +48,39 @@ function FindMatching() {
               emoji={category.emoji}
               text={category.text}
               size="lg"
-              main={true}
+              onClick={getSelectedTreatmentArea}
+              checkedData={selectedTreatmentArea}
+              setData={setSelectedTreatmentArea}
             />
           ))}
         </div>
         <div>
-          <SelectBoxDefault category="region" />
-          <SelectBoxDefault category="career" />
-          <SelectBoxDefault category="gender" />
-          <SelectBoxDefault category="filterBy" />
+          <SelectBoxDefault
+            category="userRegion"
+            getData={setSelectedRegion}
+            defaultSelectedValue="전체"
+            variants="filled"
+            size="sm"
+            width="80px"
+          />
+          <SelectBoxDefault
+            category="career"
+            getData={setSelectedCareer}
+            defaultSelectedValue={surveyedData?.preference.career.toString()}
+            size="sm"
+          />
+          <SelectBoxDefault
+            category="gender"
+            getData={setSelectedGender}
+            defaultSelectedValue={surveyedData?.preference.gender}
+            size="sm"
+          />
+          <SelectBoxDefault
+            category="filterBy"
+            getData={setSelectedFilterBy}
+            size="sm"
+            defaultSelectedValue="최신순"
+          />
         </div>
       </section>
       <section>
