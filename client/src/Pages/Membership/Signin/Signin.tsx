7cc12ed6908d54fae20fcc379065b2350968936e
heoh0613 <heoh0613@gmail.com>
2024-06-14 17:13:10 +0900

    [Feat] Made PostData States to Handle Data

--- client/src/Pages/Membership/Signin/Signin.tsx
+++ client/src/Pages/Membership/Signin/Signin.tsx
@@ -1,16 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+
+import { emailRegex, passwordRegex } from "../Signup/Signup";
 import InputText from "../../../Component/Common/Input/InputText";
 import InputPassword from "../../../Component/Common/Input/InputPassword";
 import FilledButton from "../../../Component/Common/Button/FilledButton";
 import UserTypeCheckbox from "../../../Component/Membership/UserTypeCheckbox";
+import { changeInputEvent } from "../../../Assets/CommonType/EventType";
 
 function Signin() {
-  const [userType, setUserType] = useState("부모님");
+  const [selectedUserType, setSelectedUserType] = useState<string>("parents");
+  const [postDataValue, setPostDataValue] = useState(initialPostData);
+  const isFormInvalid = validateCommonFields(postDataValue);
 
-  const handleUserType = (userType: string) => {
-    setUserType(userType);
+  useEffect(() => {
+    if (selectedUserType === "parents") {
+      setPostDataValue({ ...postDataValue, userType: "parents" });
+    } else if (selectedUserType === "therapist") {
+      setPostDataValue({ ...postDataValue, userType: "therapist" });
+    }
+  }, [selectedUserType]);
+
+  const handleChangeReducer = (e: changeInputEvent) => {
+    const { name, value } = e.target;
+    setPostDataValue({ ...postDataValue, [name]: value });
   };
+
   return (
     <main>
       <div>
@@ -24,11 +39,11 @@ function Signin() {
           <h3>회원 유형</h3>
           <span>가입했던 회원 유형을 선택 후 로그인해 주세요</span>
           <form>
-            <UserTypeCheckbox handleUserType={handleUserType} />
+            <UserTypeCheckbox onClick={setSelectedUserType} />
             <div>
-              <InputText inputType="email" />
-              <InputPassword />
-              <FilledButton text="로그인" />
+              <InputText inputType="email" dispatch={handleChangeReducer} />
+              <InputPassword dispatch={handleChangeReducer} />
+              <FilledButton text="로그인" disabled={isFormInvalid} />
             </div>
           </form>
         </div>
@@ -38,3 +53,24 @@ function Signin() {
 }
 
 export default Signin;
+
+const validateCommonFields = (data: PostDataType) => {
+  const isEmailValid = emailRegex.test(data.email);
+  const isPasswordValid = passwordRegex.test(data.password);
+
+  return data.email && data.password && isEmailValid && isPasswordValid
+    ? false
+    : true;
+};
+
+const initialPostData: PostDataType = {
+  userType: "",
+  email: "",
+  password: "",
+};
+
+interface PostDataType {
+  userType: string;
+  email: string;
+  password: string;
+}
