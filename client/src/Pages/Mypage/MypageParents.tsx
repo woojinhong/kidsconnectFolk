788121e63123b911d6ccb234aa7829442300d24b
heoh0613 <heoh0613@gmail.com>
2024-06-17 01:41:28 +0900

    [Fix] handling clicked menu states and function is added in Mypage Parent

--- client/src/Pages/Mypage/MypageParents.tsx
+++ client/src/Pages/Mypage/MypageParents.tsx
@@ -1,17 +1,33 @@
+import { useState } from "react";
+
 import Navigation from "../../Component/Mypage/Navigation/Navigation";
 import ProfileContent from "../../Component/Mypage/ParentsContent/Profile/ProfileContent";
 import MatchingContent from "../../Component/Mypage/ParentsContent/Matching/MatchingContent";
 
+import { clickButtonEvent } from "../../Assets/CommonType/EventType";
 import { StyledMypageLayout } from "../../Assets/StyledData/PageLayout.style";
 
 function MypageParents() {
+  const [clickedNavMenu, setClickedNavMenu] = useState<string>(
+    initialClickedNavMenu
+  );
+
+  const handleNavMenuClick = (e: clickButtonEvent) => {
+    setClickedNavMenu(e.currentTarget.innerText);
+  };
+
   return (
     <StyledMypageLayout>
-      <Navigation userType="therapist" />
-      <ProfileContent />
-      <MatchingContent />
+      <Navigation
+        userType="parents"
+        handleNavMenu={handleNavMenuClick}
+        clickedNavMenu={clickedNavMenu}
+      />
+      {clickedNavMenu === "내 정보" ? <ProfileContent /> : <MatchingContent />}
     </StyledMypageLayout>
   );
 }
 
 export default MypageParents;
+
+const initialClickedNavMenu: string = "내 정보";
