648bbf4b209314de76bae118a190fcb1efec13de
heoh0613 <heoh0613@gmail.com>
2024-06-21 09:21:52 +0900

    [Fix] Changed CheckBoxAgeList Props and Function to handle checkbox

--- client/src/Component/Common/CheckBox/CheckBoxAge/CheckBoxAgeList.tsx
+++ client/src/Component/Common/CheckBox/CheckBoxAge/CheckBoxAgeList.tsx
@@ -5,17 +5,26 @@ import { StyledCheckBoxContainer } from "./CheckBoxAge.style";
 
 import careChildAge from "../../../../Assets/TextData/careChildAge";
 
-function CheckBoxAgeList({ checked = false }: { checked?: boolean }) {
+function CheckBoxAgeList({
+  checkedData,
+  setData,
+  onChange,
+}: CheckBoxAgeListProps) {
   const [ageValue, setAgeValue] = useState<string[]>([]);
 
+  const handleOnChange = (value: string[]) => {
+    setAgeValue(value);
+    onChange && onChange(value);
+  };
   return (
-    <StyledCheckBoxContainer value={ageValue} onChange={setAgeValue}>
+    <StyledCheckBoxContainer value={ageValue} onChange={handleOnChange}>
       {careChildAge.map((data) => (
         <CheckBoxAgeBox
           key={data.label}
           label={data.label}
           description={data.description}
-          checked={checked}
+          checkedData={checkedData}
+          setData={setData}
         />
       ))}
     </StyledCheckBoxContainer>
@@ -23,3 +32,9 @@ function CheckBoxAgeList({ checked = false }: { checked?: boolean }) {
 }
 
 export default CheckBoxAgeList;
+
+type CheckBoxAgeListProps = {
+  checkedData: string[];
+  setData: React.Dispatch<React.SetStateAction<string[]>>;
+  onChange?: (value: string[]) => void;
+};
