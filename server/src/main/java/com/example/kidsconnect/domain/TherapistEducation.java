c36c6495762110671da607c77e6ba6f261e2dff0
woojin <orolzleim@gmail.com>
2024-06-30 19:57:15 +0900

    add education field

--- server/src/main/java/com/example/kidsconnect/domain/TherapistEducation.java
+++ server/src/main/java/com/example/kidsconnect/domain/TherapistEducation.java
@@ -19,6 +19,8 @@ public class TherapistEducation {
     private Long id;
     private String education;
     private String degree;
+    @Column(nullable = false, length = 30)
+    private String status;
 
 
 }
