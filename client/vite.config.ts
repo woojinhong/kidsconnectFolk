cd62a9507f38f47efb78146742c47910f4bd2927
woojin <orolzleim@gmail.com>
2024-07-18 23:36:25 +0900

    vite.config external access approval

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // 외부에서 접근할 수 있도록 설정
+  }
 })
